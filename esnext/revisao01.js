60e2e3fcbf65b82162ac6f7859d2ca999a4952f6
fabiocostadeoliveira <fabio_unipar@hotmail.com.br>
2019-01-14 13:13:45 -0200

    entrada e saida padrao

---
+++ esnext/revisao01.js
@@ -0,0 +1,12 @@
+{
+    var a = 'a'
+    let b = 'b'
+    console.log(b)
+}
+//console.log(b) /// ISSO NAO PODE, POIS FOI DEFINIDA DENTRO DO BLOCO
+console.log(a)
+
+// template string
+
+const produto = 'Ipad'
+console.log(`${produto} é caro !!`)
